e81d8049ecbb7508fbc4ef7b79228aa265215395
Mateus Cardozo Gomes da Silva <mcgs@cin.ufpe.br>
2018-04-15 23:59:47 -0300

    Passo 17 - Trocando de ListView para RecyclerView

--- RSS/app/src/main/java/br/ufpe/cin/if1001/rss/ui/MainActivity.java
+++ RSS/app/src/main/java/br/ufpe/cin/if1001/rss/ui/MainActivity.java
@@ -7,146 +7,108 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
-import android.util.Log;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.CursorAdapter;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 import br.ufpe.cin.if1001.rss.R;
-import br.ufpe.cin.if1001.rss.db.RssProviderContract;
 import br.ufpe.cin.if1001.rss.db.SQLiteRSSHelper;
 import br.ufpe.cin.if1001.rss.services.DownloadXmlService;
 
 public class MainActivity extends Activity {
 
-    private ListView conteudoRSS;
-    private final String RSS_FEED = "http://rss.cnn.com/rss/edition.rss";
-    private SQLiteRSSHelper db;
+  private RecyclerView conteudoRSS;
+  private final String RSS_FEED = "http://rss.cnn.com/rss/edition.rss";
+  private SQLiteRSSHelper db;
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    db = SQLiteRSSHelper.getInstance(this);
+
+    conteudoRSS = findViewById(R.id.conteudoRSS);
+
+    conteudoRSS.setLayoutManager(new LinearLayoutManager(this));
+    conteudoRSS.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+    conteudoRSS.setItemAnimator(new DefaultItemAnimator());
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+    String linkfeed = preferences.getString("rssfeedlink", getResources().getString(R.string.rssfeed));
+    Intent downloadService = new Intent(getApplicationContext(), DownloadXmlService.class);
+    downloadService.putExtra("url", linkfeed);
+    startService(downloadService);
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+    IntentFilter intentFilter = new IntentFilter(DownloadXmlService.DOWNLOAD_COMPLETE);
+    LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(onDownloadCompleteEvent, intentFilter);
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    LocalBroadcastManager.getInstance(getApplicationContext()).unregisterReceiver(onDownloadCompleteEvent);
+  }
+
+  private BroadcastReceiver onDownloadCompleteEvent = new BroadcastReceiver() {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        db = SQLiteRSSHelper.getInstance(this);
-
-        conteudoRSS = findViewById(R.id.conteudoRSS);
-
-        SimpleCursorAdapter adapter =
-                new SimpleCursorAdapter(
-                        //contexto, como estamos acostumados
-                        this,
-                        //Layout XML de como se parecem os itens da lista
-                        R.layout.item,
-                        //Objeto do tipo Cursor, com os dados retornados do banco.
-                        //Como ainda não fizemos nenhuma consulta, está nulo.
-                        null,
-                        //Mapeamento das colunas nos IDs do XML.
-                        // Os dois arrays a seguir devem ter o mesmo tamanho
-                        new String[]{SQLiteRSSHelper.ITEM_TITLE, SQLiteRSSHelper.ITEM_DATE},
-                        new int[]{R.id.itemTitulo, R.id.itemData},
-                        //Flags para determinar comportamento do adapter, pode deixar 0.
-                        0
-                );
-        //Seta o adapter. Como o Cursor é null, ainda não aparece nada na tela.
-        conteudoRSS.setAdapter(adapter);
-
-        // permite filtrar conteudo pelo teclado virtual
-        conteudoRSS.setTextFilterEnabled(true);
-
-        //Complete a implementação deste método de forma que ao clicar, o link seja aberto no navegador e
-        // a notícia seja marcada como lida no banco
-        conteudoRSS.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                SimpleCursorAdapter adapter = (SimpleCursorAdapter) parent.getAdapter();
-                Cursor mCursor = ((Cursor) adapter.getItem(position));
-                db.markAsRead(mCursor.getString(mCursor.getColumnIndexOrThrow(RssProviderContract.LINK)));
-                Intent seeOnBrowser = new Intent(Intent.ACTION_VIEW);
-                Uri itemUrl = Uri.parse(mCursor.getString(mCursor.getColumnIndexOrThrow(RssProviderContract.LINK)));
-                seeOnBrowser.setData(itemUrl);
-                if (seeOnBrowser.resolveActivity(getPackageManager()) != null) {
-                    startActivity(seeOnBrowser);
-                }
-            }
-        });
+    public void onReceive(Context context, Intent intent) {
+      Toast.makeText(getApplicationContext(), "News loaded, displaying feed.", Toast.LENGTH_LONG).show();
+      new ExibirFeed().execute();
     }
+  };
+
+  @Override
+  protected void onDestroy() {
+    db.close();
+    super.onDestroy();
+  }
+
+  class ExibirFeed extends AsyncTask<Void, Void, Cursor> {
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String linkfeed = preferences.getString("rssfeedlink", getResources().getString(R.string.rssfeed));
-        Intent downloadService = new Intent(getApplicationContext(), DownloadXmlService.class);
-        downloadService.putExtra("url", linkfeed);
-        startService(downloadService);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        IntentFilter intentFilter = new IntentFilter(DownloadXmlService.DOWNLOAD_COMPLETE);
-        LocalBroadcastManager.getInstance(getApplicationContext()).registerReceiver(onDownloadCompleteEvent, intentFilter);
+    protected Cursor doInBackground(Void... voids) {
+      Cursor c = db.getItems();
+      c.getCount();
+      return c;
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        LocalBroadcastManager.getInstance(getApplicationContext()).unregisterReceiver(onDownloadCompleteEvent);
+    protected void onPostExecute(Cursor c) {
+      if (c != null) {
+        conteudoRSS.setAdapter(new RecyclerAdapter(getApplicationContext(), c));
+      }
     }
+  }
 
-    private BroadcastReceiver onDownloadCompleteEvent = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Toast.makeText(getApplicationContext(), "News loaded, displaying feed.", Toast.LENGTH_LONG).show();
-            new ExibirFeed().execute();
-        }
-    };
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.mainmenu, menu);
+    return super.onCreateOptionsMenu(menu);
+  }
 
-    @Override
-    protected void onDestroy() {
-        db.close();
-        super.onDestroy();
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+    case R.id.btn_Config:
+      startActivity(new Intent(this, ConfigActivity.class));
+      return true;
     }
-
-    class ExibirFeed extends AsyncTask<Void, Void, Cursor> {
-
-        @Override
-        protected Cursor doInBackground(Void... voids) {
-            Cursor c = db.getItems();
-            c.getCount();
-            return c;
-        }
-
-        @Override
-        protected void onPostExecute(Cursor c) {
-            if (c != null) {
-                ((CursorAdapter) conteudoRSS.getAdapter()).changeCursor(c);
-            }
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.mainmenu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.btn_Config:
-                startActivity(new Intent(this, ConfigActivity.class));
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    return super.onOptionsItemSelected(item);
+  }
 }
